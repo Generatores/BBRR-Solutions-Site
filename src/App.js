748c04c76066a90dfb28ee30be4a216269fdfeae
Bruno Ruiz <Bruno.Ruiz@spectrumplastics.com>
2021-04-05 16:00:48 -0700

    creation of master branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Stdbtn from "./components/std-button";
+import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <h1>BBRR Solutions React Boilerplate</h1>
+      <div className='container'>
+        <p>Please enjoy our version of create-react-app</p>
+        <Stdbtn />
+        <Stdbtn />
+        <Stdbtn />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Visit us at{" "}
+          <a href='https://dev.bbrr.solutions'>dev.bbrr.solutions</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          See our public repositories at{" "}
+          <a href='https://dev.azure.com/BBRRSolutions/BBRR%20DevOps'>
+            BBRR DevOps
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/components/std-button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./std-button.css";
+
+export default class Stdbtn extends React.Component {
+  render() {
+    return (
+      <div className='button_container'>
+        <button className='std_button'>Standard button</button>
+      </div>
+    );
+  }
+}
